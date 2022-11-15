c864c3767eb6860b180bdc8c247dc80af1970e34
Saehwa95 <tpghk1002@naver.com>
2022-11-15 21:02:53 +0900

    남은 작업 - 서버 : userId 활용 작업 - css, mongoDB 추후 작업 필요!

--- app.py
+++ app.py
@@ -8,17 +8,23 @@ client = MongoClient('mongodb+srv://test:sparta@cluster0.fjqxvob.mongodb.net/?re
 db = client.dbsparta
 
 
+# 라우트 작업
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html')
 
 
 @app.route('/postAdd')
 def post_page():
     return render_template('postAdd.html')
 
-
-# 게시글 작성
+# 기능 구현 작업
+# 1. 게시글 작성
 @app.route('/api/post', methods=['POST'])
 def post_add():
     content_receive = request.form['content_give']
@@ -26,6 +32,7 @@ def post_add():
     doc = {'content': content_receive}
 
     db.content.insert_one(doc)
+
     return jsonify({'msg': '작성 완료!'})
 
 
