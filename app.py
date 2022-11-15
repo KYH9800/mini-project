3d4dc4173c546830b3fe9b20685d3b9786a0997e
Saehwa95 <tpghk1002@naver.com>
2022-11-15 19:03:44 +0900

    게시글 작성 작업 완료 css, mongoDB 추후 작업 필요!

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.fjqxvob.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
 
 @app.route('/')
@@ -8,19 +13,20 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-
 @app.route('/postAdd')
-def post_add():
+def post_page():
     return render_template('postAdd.html')
+
+
+# 게시글 작성
+@app.route('/api/post', methods=['POST'])
+def post_add():
+    content_receive = request.form['content_give']
+
+    doc = {'content': content_receive}
+
+    db.content.insert_one(doc)
+    return jsonify({'msg': '작성 완료!'})
 
 
 if __name__ == '__main__':
