9447c2757891ba6a7d1d4a7614fa67d35cf06867
고윤혁 <sksgur3217@gmail.com>
2022-11-15 11:38:45 +0900

    start!!

--- app.py
+++ app.py
@@ -18,5 +18,10 @@ def signup():
     return render_template('signup.html')
 
 
+@app.route('/postAdd')
+def post_add():
+    return render_template('postAdd.html')
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
